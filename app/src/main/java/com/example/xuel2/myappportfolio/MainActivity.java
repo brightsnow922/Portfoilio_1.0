2a44f454a5f2c1b73413cb431753da364d891228
Liang <xueliang_1980@hotmail.com>
2016-08-20 18:42:36 +0800

    Initial commit

--- app/src/main/java/com/example/xuel2/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/xuel2/myappportfolio/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.xuel2.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToastPopularMovie(View view) {
+        char abc = 'a';
+        Toast toast = Toast.makeText(getApplicationContext(),"Popular Movie Message", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP,20,300);
+        toast.show();
     }
 }
